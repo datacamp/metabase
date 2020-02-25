48562c260d4ef82ca5631e3261a378311788540b
Gaëtan Podevijn <gaetan@datacamp.com>
2020-02-25 16:02:50 +0100

    redshift spectrum driver

---
+++ modules/drivers/redshiftspectrum/test/metabase/driver/redshift_test.clj
@@ -0,0 +1,18 @@
+(ns metabase.driver.redshiftspectrum-test
+  (:require [clojure.test :refer :all]
+            [metabase.plugins.jdbc-proxy :as jdbc-proxy]
+            [metabase.test
+             [fixtures :as fixtures]
+             [util :as tu]]
+            [metabase.test.data.datasets :refer [expect-with-driver]]))
+
+(use-fixtures :once (fixtures/initialize :plugins))
+
+(expect-with-driver :redshiftspectrum
+  "UTC"
+  (tu/db-timezone-id))
+
+(deftest correct-driver-test
+  (is (= "com.amazon.redshift.jdbc.Driver"
+         (.getName (class (jdbc-proxy/wrapped-driver (java.sql.DriverManager/getDriver "jdbc:redshift://host:5432/testdb")))))
+      "Make sure we're using the correct driver for Redshift"))
